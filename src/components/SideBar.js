4d63037ac2b1d81d617684053b61571322845e73
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-29 16:58:07 +0530

    segregated current user and other users:

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -46,7 +46,7 @@ const SideBar = () => {
 
   return (
     <div className="w-2/12 h-full overflow-scroll bg-blue-400">
-      <div className="flex text-white text-lg items-center my-5 ml-4">
+      <div className="flex text-white text-lg items-center my-4 ml-4">
         <i class="fa-brands fa-rocketchat fa-beat fa-lg"></i>
         <h3 className="ml-3"> Room Name </h3>
       </div>
@@ -54,34 +54,47 @@ const SideBar = () => {
         <span className="ml-3 capitalize">{room}</span>
       </div>
 
+      <div className="flex text-white text-lg items-center my-4 ml-4">
+        <i class="fa-solid fa-user fa-lg"></i>{" "}
+        <h3 className="ml-3"> Current User </h3>
+      </div>
+      <div className="flex text-white text-lg  bg-blue-500 p-2">
+        <span className="ml-3 capitalize">{loggedInUser.name}</span>
+      </div>
+
       <div className="my-10">
         <h3 className="flex text-white text-lg ml-4 my-5 items-center">
           {" "}
           <i class="fa-solid fa-user fa-lg"></i>{" "}
-          <span className="ml-3"> Users </span>
+          <span className="ml-3"> Other Users </span>
         </h3>
+
         <ul className=" flex text-white text-lg  my-5 flex-col">
           {users.map((user) => {
-            return (
-              <li className="flex ml-5 items-center ">
-                <div className="my-1">{user.name}</div>
+            if (user.name !== loggedInUser.name) {
+              return (
+                <li className="flex ml-5 items-center ">
+                  <div className="my-1">{user.name}</div>
 
-                <div className="ml-2">
-                  <span class="relative flex h-3 w-3">
-                    <span
-                      className={`animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 ${
-                        user.online ? "bg-[#0fcc45]" : "bg-[#cc0f0f]"
-                      }`}
-                    ></span>
-                    <span
-                      className={`relative inline-flex rounded-full h-3 w-3 ${
-                        user.online ? "bg-[#0fcc45]" : "bg-[#cc0f0f]"
-                      }`}
-                    ></span>
-                  </span>
-                </div>
-              </li>
-            );
+                  <div className="ml-2">
+                    <span class="relative flex h-3 w-3">
+                      <span
+                        className={`animate-ping absolute inline-flex h-full w-full rounded-full opacity-75 ${
+                          user.online ? "bg-[#0fcc45]" : "bg-[#cc0f0f]"
+                        }`}
+                      ></span>
+                      <span
+                        className={`relative inline-flex rounded-full h-3 w-3 ${
+                          user.online ? "bg-[#0fcc45]" : "bg-[#cc0f0f]"
+                        }`}
+                      ></span>
+                    </span>
+                  </div>
+                </li>
+              );
+            } else {
+              return null;
+            }
           })}
         </ul>
       </div>
