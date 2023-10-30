ab283322e2e304a77a512f511bb8a84ae2fb7da0
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-31 02:32:17 +0530

    added chat logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { ConversationsContext } from "../contexts/ConversationProvider";
 import { SocketContext } from "../contexts/SocketProvider";
 import { UsersContext } from "../contexts/UsersProvider";
-
+// import chatimg from "./chat.webp";
 const Header = () => {
   const { socket, createSocketConnection } = useContext(SocketContext);
   const { loggedInUser } = useContext(UsersContext);
@@ -17,7 +17,14 @@ const Header = () => {
   };
   return (
     <div className="flex justify-between w-full h-full bg-black">
-      <div className="flex md:text-[2rem] text-[0.7rem] md:my-0 my-4 text-white md:p-4 p-0 mx-8">
+      <div
+        className="flex md:text-[2rem] text-[0.7rem] md:my-0 my-4 text-white md:p-4 p-0 cursor-pointer"
+        onClick={reset}
+      >
+        <img
+          src="../chat.webp"
+          className="md:w-16 md:h-12 w-2 h-2 rounded-[0.8rem] mr-2"
+        />
         ChatApp
       </div>
       <div className=" flex justify-center items-center p-4 text-white ">
