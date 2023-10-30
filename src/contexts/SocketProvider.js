b7cfec767e93c05f6af10e38031a7dccc71fbe4a
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-31 02:43:22 +0530

    added sBE server url

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -1,19 +1,23 @@
 import React, { createContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 export const SocketContext = createContext();
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://chat-server-qgnm.onrender.com"
+    : "http://localhost:5600";
 
 export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState();
 
   const createSocketConnection = () => {
-    const newSocket = io("http://localhost:5600");
+    const newSocket = io(URL);
     newSocket.on("connect", () => {
       setSocket(newSocket);
     });
   };
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5600");
+    const newSocket = io(URL);
     newSocket.on("connect", () => {
       setSocket(newSocket);
     });
